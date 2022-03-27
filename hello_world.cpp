e5048997e86ce556a8521d55b6c39508d83c708a
ShapArt <artemshapovalov2072@mail.ru>
2022-03-27 22:46:52 +0300

    Программа выводит Hello Word from

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,10 +1,9 @@
 #include <iostream>
-
 using namespace std;
-
-int main()
-{
-    cout<<"Hello World";
-
-    return 0;
+int main(){
+   char* name = "";
+   cout << "Your name is: ";
+   cin >> name;
+   cout << "Hello world from" << name << endl;
+   return 0;
 }
